8ba743adb37db1a4d7d350b683e337c4424f3785
edikai <38525618+edikai@users.noreply.github.com>
2025-02-07 13:27:36 +0800

    Update GenUtils.java
    格式化

--- platform-gen/src/main/java/com/platform/utils/GenUtils.java
+++ platform-gen/src/main/java/com/platform/utils/GenUtils.java
@@ -21,7 +21,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
- * 代码生成器   工具类
+ * 代码生成器工具类
  *
  * @author lipengjun
  * @email 939961241@qq.com
@@ -51,7 +51,6 @@ public class GenUtils {
                                      List<Map<String, String>> columns, ZipOutputStream zip) {
         //配置信息
         Configuration config = getConfig();
-
         //表信息
         TableEntity tableEntity = new TableEntity();
         tableEntity.setTableName(table.get("tableName"));
@@ -61,7 +60,6 @@ public class GenUtils {
         String className = tableToJava(tableEntity.getTableName(), tablePrefix);
         tableEntity.setClassName(className);
         tableEntity.setClassname(StringUtils.uncapitalize(className));
-
         //列信息
         List<ColumnEntity> columsList = new ArrayList<>();
         boolean hasDate = false;
@@ -72,16 +70,13 @@ public class GenUtils {
             columnEntity.setDataType(column.get("dataType"));
             columnEntity.setComments(column.get("columnComment"));
             columnEntity.setExtra(column.get("extra"));
-
             //列名转换成Java属性名
             String attrName = columnToJava(columnEntity.getColumnName());
             columnEntity.setAttrName(attrName);
             columnEntity.setAttrname(StringUtils.uncapitalize(attrName));
-
             //列的数据类型，转换成Java类型
             String attrType = config.getString(columnEntity.getDataType(), "String");
             columnEntity.setAttrType(attrType);
-
             if ("Date".equals(attrType)) {
                 hasDate = true;
             }
@@ -98,11 +93,9 @@ public class GenUtils {
                     tableEntity.setPk(columnEntity);
                 }
             }
-
             columsList.add(columnEntity);
         }
         tableEntity.setColumns(columsList);
-
         //若没主键
         if (tableEntity.getPk() == null) {
             //设置columnName为id的为主键
@@ -119,14 +112,11 @@ public class GenUtils {
                 tableEntity.setPk(tableEntity.getColumns().get(0));
             }
         }
-
         String pre = tablePrefix.replace("_", "").toLowerCase();
-
         //设置velocity资源加载器
         Properties prop = new Properties();
         prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         Velocity.init(prop);
-
         //封装模板数据
         Map<String, Object> map = new HashMap<>();
         map.put("tableName", tableEntity.getTableName());
@@ -143,7 +133,6 @@ public class GenUtils {
         map.put("hasBigDecimal", hasBigDecimal);
         map.put("pre", pre);
         VelocityContext context = new VelocityContext(map);
-
         //获取模板列表
         List<String> templates = getTemplates();
         for (String template : templates) {
@@ -151,7 +140,6 @@ public class GenUtils {
             StringWriter sw = new StringWriter();
             Template tpl = Velocity.getTemplate(template, "UTF-8");
             tpl.merge(context, sw);
-
             try {
                 //添加到zip
                 zip.putNextEntry(new ZipEntry(getFileName(template, tableEntity.getClassName(), config.getString("package"), pre)));
@@ -196,49 +184,38 @@ public class GenUtils {
      * 获取文件名
      */
     private static String getFileName(String template, String className, String packageName, String tablePrefix) {
-
         String packagePath = "main" + File.separator + "java" + File.separator;
         if (StringUtils.isNotBlank(packageName)) {
             packagePath += packageName.replace(".", File.separator) + File.separator;
         }
-
         if (template.contains("Entity.java.vm")) {
             return packagePath + "entity" + File.separator + className + "Entity.java";
         }
-
         if (template.contains("Dao.java.vm")) {
             return packagePath + "dao" + File.separator + className + "Dao.java";
         }
-
         if (template.contains("Dao.xml.vm")) {
             return packagePath + "dao" + File.separator + className + "Dao.xml";
         }
-
         if (template.contains("Service.java.vm")) {
             return packagePath + "service" + File.separator + className + "Service.java";
         }
-
         if (template.contains("ServiceImpl.java.vm")) {
             return packagePath + "service" + File.separator + "impl" + File.separator + className + "ServiceImpl.java";
         }
-
         if (template.contains("Controller.java.vm")) {
             return packagePath + "controller" + File.separator + className + "Controller.java";
         }
-
         if (template.contains("list.html.vm")) {
             return "main" + File.separator + "webapp" + File.separator + "WEB-INF" + File.separator + "page"
                     + File.separator + tablePrefix + File.separator + className.toLowerCase() + ".html";
         }
-
         if (template.contains("list.js.vm")) {
             return "main" + File.separator + "webapp" + File.separator + "js" + File.separator + tablePrefix + File.separator + className.toLowerCase() + ".js";
         }
-
         if (template.contains("menu.sql.vm")) {
             return className.toLowerCase() + "_menu.sql";
         }
-
         return null;
     }
 }
